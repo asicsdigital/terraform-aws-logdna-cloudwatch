3b876b8405d7a3672df4269c1a46b2e683d39ac4
Tim Hartmann <tfhartmann@gmail.com>
2018-09-07 16:07:55 -0400

    Reorder var file

--- variables.tf
+++ variables.tf
@@ -1,28 +1,3 @@
-variable "service_identifier" {
-  description = "service_identifier - Unique identifier for the app, used in naming resources. (ex: LogDNA-dev )"
-}
-
-#variable "logdna_key" {
-#  description = "logdna_key - LogDNA Ingestion key. LOGDNA_KEY"
-#}
-
-variable "reserved_concurrent_executions" {
-  type        = "string"
-  description = "Number of reserved concurrent executions (default 10)"
-  default     = "10"
-}
-
-variable "url" {
-  description = "URL to script content. Defaults to GitHub Master"
-  default     = "https://raw.githubusercontent.com/logdna/aws-cloudwatch/master/logdna_cloudwatch.py"
-}
-
-#
-#Environment variables:
-#(Required) LOGDNA_KEY: LOGDNA_KEY YOUR_INGESTION_KEY_HERE
-#(Optional) LOGDNA_HOSTNAME: Alternative Host Name
-#(Optional) LOGDNA_TAGS: Comma-separated Tags
-
 variable "logdna_hostname" {
   description = "LOGDNA_HOSTNAME Alternative Host Name"
   default     = ""
@@ -36,4 +11,19 @@ variable "logdna_tags" {
   type        = "list"
   description = "List of tags to add to log DNA, current region is always added"
   default     = []
+}
+
+variable "reserved_concurrent_executions" {
+  type        = "string"
+  description = "Number of reserved concurrent executions (default 10)"
+  default     = "10"
+}
+
+variable "service_identifier" {
+  description = "service_identifier - Unique identifier for the app, used in naming resources. (ex: LogDNA-dev )"
+}
+
+variable "url" {
+  description = "URL to script content. Defaults to GitHub Master"
+  default     = "https://raw.githubusercontent.com/logdna/aws-cloudwatch/master/logdna_cloudwatch.py"
 }
