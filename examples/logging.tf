0c292da13322cb3ed06325059483964b93012268
Tim Hartmann <tfhartmann@gmail.com>
2018-09-10 11:13:58 -0400

    Formatting

--- examples/logging.tf
+++ examples/logging.tf
@@ -7,14 +7,14 @@ module "logdna" {
 resource "aws_cloudwatch_log_subscription_filter" "logdna_lambdafunction_logfilter" {
   name            = "test_lambdafunction_logfilter"
   log_group_name  = "some-log-group-name"
-  filter_pattern  = "" # This is required, but can be empty
+  filter_pattern  = ""                              # This is required, but can be empty
   destination_arn = "${module.logdna.lambda_arn}"
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch" {
-  statement_id   = "AllowExecutionFromCloudWatch-${module.logdna.function_name}-${data.aws_region.current.name}"
-  action         = "lambda:InvokeFunction"
-  function_name  = "${module.logdna.function_name}"
-  principal      = "logs.amazonaws.com"
-  source_arn     = "arn:aws:logs:us-east-1:123456789012:log-group:some-log-group-name:*"
+  statement_id  = "AllowExecutionFromCloudWatch-${module.logdna.function_name}-${data.aws_region.current.name}"
+  action        = "lambda:InvokeFunction"
+  function_name = "${module.logdna.function_name}"
+  principal     = "logs.amazonaws.com"
+  source_arn    = "arn:aws:logs:us-east-1:123456789012:log-group:some-log-group-name:*"
 }
