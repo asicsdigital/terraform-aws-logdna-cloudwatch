bfffdc6f5e78946de32c20e02453ab1ce2c56e9e
Tim Hartmann <tfhartmann@gmail.com>
2018-09-10 13:32:12 -0400

    Second try at a dependecy fix

--- main.tf
+++ main.tf
@@ -49,8 +49,8 @@ resource "aws_iam_role_policy_attachment" "xray_wo" {
 
 resource "aws_lambda_function" "logdna_cloudwatch" {
   description                    = "AWS Lambda for logging into LogDNA"
-  filename                       = "${data.archive_file.logdna_cloudwatch.filename}"
-  source_code_hash               = "${base64sha256(file("${data.archive_file.logdna_cloudwatch.filename}"))}"
+  filename                       = "${local.output_file}"
+  source_code_hash               = "${data.archive_file.logdna_cloudwatch.output_base64sha256}"
   function_name                  = "${local.service_identifier}"
   role                           = "${aws_iam_role.lambda_execution_role.arn}"
   handler                        = "logdna_cloudwatch.lambda_handler"
