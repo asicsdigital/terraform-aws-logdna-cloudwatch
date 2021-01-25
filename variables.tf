93a7fb4bfdab98f2628ecdaaf35976de6c5617b3
John Noss <john.noss@asics.com>
2021-01-25 16:41:15 -0500

    revert #10 and #11: node lambda Source/App bug
    The LogDNA node lambda has a bug where Source and App are reversed, so
    rolling back to python one. Will open a new PR for adding the boto
    python layer from AWS to maintain python boto requests library

--- variables.tf
+++ variables.tf
@@ -23,6 +23,11 @@ variable "service_identifier" {
   description = "service_identifier - Unique identifier for the app, used in naming resources. (ex: LogDNA-dev )"
 }
 
+variable "url" {
+  description = "URL to script content. Defaults to GitHub Master"
+  default     = "https://raw.githubusercontent.com/logdna/aws-cloudwatch/0.1.0/logdna_cloudwatch.py"
+}
+
 variable "lambda_timeout" {
   description = "Timeout in seconds for the lambda"
   default     = 3
